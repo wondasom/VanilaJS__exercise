e84bccf4674d083609f2d2b5c64340fafc55b933
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-21 18:17:08 +0200

    do refactoring by event delegation

--- shopping-list/main.js
+++ shopping-list/main.js
@@ -8,48 +8,35 @@ const onAdd = () => {
 		input.focus();
 		return;
 	}
-
 	const item = createItem(text);
-
 	items.appendChild(item);
-
 	item.scrollIntoView({ block: "center" });
-
 	input.value = "";
 	input.focus();
 };
 
-addBtn.addEventListener("click", () => {
-	onAdd();
+items.addEventListener("click", (event) => {
+	const id = event.target.dataset.id;
+	if (id) {
+		const toBeDeleted = document.querySelector(`.item__row[data-id="${id}"]`);
+		toBeDeleted.remove()
+	}
 });
 
+let index = 0;
 const createItem = (text) => {
 	const itemRow = document.createElement("li");
 	itemRow.setAttribute("class", "item__row");
-
-	const item = document.createElement("div");
-	item.setAttribute("class", "item");
-
-	const name = document.createElement("span");
-	name.setAttribute("class", "item__name");
-	name.innerText = text;
-
-	const deleteBtn = document.createElement("button");
-	deleteBtn.setAttribute("class", "item__delete");
-	deleteBtn.innerHTML = `<i class="far fa-trash-alt"></i>`;
-	deleteBtn.addEventListener("click", () => {
-		items.removeChild(itemRow);
-	});
-
-	const itemDivder = document.createElement("div");
-	itemDivder.setAttribute("class", "item__divider");
-
-	item.appendChild(name);
-	item.appendChild(deleteBtn);
-
-	itemRow.appendChild(item);
-	itemRow.appendChild(itemDivder);
-
+	itemRow.setAttribute("data-id", index);
+	itemRow.innerHTML = `			
+	<div class="item" >
+		<span class="item__name">${text}</span>
+		<button class="item__delete">
+			<i class="far fa-trash-alt" data-id=${index}></i>
+		</button>
+	</div>
+	<div class="item__divider"></div>`;
+	index++;
 	return itemRow;
 };
 
