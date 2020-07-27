cdd8a4ccd70fcaa4057084cf8b0dbedd880d923e
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-27 17:25:57 +0200

    display carrots and bugs randomly within the game field

---
+++ carrot-game/main.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const CARROT_SIZE = 80;
+const field = document.querySelector(".game__field");
+const fieldRect = field.getBoundingClientRect();
+
+const initGame = () => {
+	// generate carrot, bugs and add them to the field
+	console.log(fieldRect);
+	addItem("carrot", 5, "img/carrot.png");
+	addItem("bug", 5, "img/bug.png");
+};
+
+const addItem = (className, count, imgPath) => {
+	const x1 = 0;
+	const y1 = 0;
+	const x2 = fieldRect.width - CARROT_SIZE;
+	const y2 = fieldRect.height - CARROT_SIZE;
+	for (let i = 0; i < count; i++) {
+		const item = document.createElement("img");
+		item.setAttribute("class", className);
+		item.setAttribute("src", imgPath);
+		item.style.position = "absolute";
+		const x = randomNumber(x1, x2);
+		const y = randomNumber(y1, y2);
+		item.style.left = `${x}px`;
+		item.style.top = `${y}px`;
+		field.appendChild(item);
+	}
+};
+
+const randomNumber = (min, max) => {
+	return Math.random() * (max - min) + min;
+};
+
+initGame();
