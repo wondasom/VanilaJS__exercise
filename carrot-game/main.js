afa8034e5dc1a83bf80a3edb179a4e7dd5d2d3b5
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-27 22:00:44 +0200

    add game logics

--- carrot-game/main.js
+++ carrot-game/main.js
@@ -1,14 +1,129 @@
 "use strict";
 
 const CARROT_SIZE = 80;
+const CARROT_COUNT = 20;
+const BUG_COUNT = 10;
+const GAME_DURATION_SEC = 10;
+
 const field = document.querySelector(".game__field");
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector(".game__button");
+const gameTimer = document.querySelector(".game__timer");
+const gameScore = document.querySelector(".game__score");
+
+const popUp = document.querySelector(".pop-up");
+const popUpText = document.querySelector(".pop-up__message");
+const popUpRefresh = document.querySelector(".pop-up__refresh");
+
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
+
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener("click", () => {
+	if (started) {
+		stopGame();
+	} else {
+		startGame();
+	}
+});
+
+popUpRefresh.addEventListener("click", () => {
+	startGame();
+	hidePopUp();
+});
+
+const startGame = () => {
+	started = true;
+	initGame();
+	showStopButton();
+	showTimerAndScore();
+	startGameTimer();
+	playSound(bgSound);
+};
+
+const stopGame = () => {
+	started = false;
+	stopGameTimer();
+	hideGameButton();
+	showPopUpWithText("REPLAY❓");
+	stopSound(bgSound);
+	playSound(alertSound);
+};
+
+const finishGame = (win) => {
+	started = false;
+	hideGameButton();
+	if (win) {
+		playSound(winSound);
+	} else {
+		playSound(bugSound);
+	}
+	stopGameTimer();
+	stopSound(bgSound);
+	showPopUpWithText(win ? "YOU WON 🎉" : "YOU LOST 💩");
+};
+
+const showStopButton = () => {
+	const icon = gameBtn.querySelector(".fas");
+	icon.classList.add("fa-stop");
+	icon.classList.remove("fa-play");
+	gameBtn.style.visibility = "visible";
+};
+
+const hideGameButton = () => {
+	gameBtn.style.visibility = "hidden";
+};
+
+const showTimerAndScore = () => {
+	gameTimer.style.visibility = "visible";
+	gameScore.style.visibility = "visible";
+};
+
+const startGameTimer = () => {
+	let remainingTimeSec = GAME_DURATION_SEC;
+	updateTimerText(remainingTimeSec);
+	timer = setInterval(() => {
+		if (remainingTimeSec <= 0) {
+			clearInterval(timer);
+			finishGame(CARROT_COUNT === score);
+			return;
+		}
+		updateTimerText(--remainingTimeSec);
+	}, 1000);
+};
+
+const stopGameTimer = () => {
+	clearInterval(timer);
+};
+
+const updateTimerText = (time) => {
+	const minutes = Math.floor(time / 60);
+	const seconds = time % 60;
+	gameTimer.innerText = `${minutes}:${seconds}`;
+};
+
+const showPopUpWithText = (text) => {
+	popUpText.innerText = text;
+	popUp.classList.remove("pop-up--hide");
+};
+
+const hidePopUp = () => {
+	popUp.classList.add("pop-up--hide");
+};
 
 const initGame = () => {
+	score = 0;
+	field.innerHTML = "";
+	gameScore.innerText = CARROT_COUNT;
 	// generate carrot, bugs and add them to the field
-	console.log(fieldRect);
-	addItem("carrot", 5, "img/carrot.png");
-	addItem("bug", 5, "img/bug.png");
+	addItem("carrot", CARROT_COUNT, "img/carrot.png");
+	addItem("bug", BUG_COUNT, "img/bug.png");
 };
 
 const addItem = (className, count, imgPath) => {
@@ -33,4 +148,34 @@ const randomNumber = (min, max) => {
 	return Math.random() * (max - min) + min;
 };
 
-initGame();
+const onFieldClick = (event) => {
+	if (!started) {
+		return;
+	}
+	const target = event.target;
+	if (target.matches(".carrot")) {
+		target.remove();
+		score++;
+		playSound(carrotSound);
+		updateScoreBoard();
+		if (score === CARROT_COUNT) {
+			finishGame(true);
+		}
+	} else if (target.matches(".bug")) {
+		finishGame(false);
+	}
+};
+
+const playSound = (sound) => {
+	sound.currentTime = 0;
+	sound.play();
+};
+
+const stopSound = (sound) => {
+	sound.pause();
+};
+const updateScoreBoard = () => {
+	gameScore.innerText = CARROT_COUNT - score;
+};
+
+field.addEventListener("click", onFieldClick);
