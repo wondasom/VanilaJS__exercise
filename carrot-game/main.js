377abee50329484f079e0faaa05bf133adce2c94
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-28 14:09:31 +0200

    LOGIC_REVIEW add functions for the sound effect

--- carrot-game/main.js
+++ carrot-game/main.js
@@ -15,6 +15,12 @@ const popUp = document.querySelector(".pop-up");
 const popUpText = document.querySelector(".pop-up__message");
 const popUpRefresh = document.querySelector(".pop-up__refresh");
 
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const alertSound = new Audio("./sound/alert.wav");
+const bgSound = new Audio("./sound/bg.mp3");
+const bugSound = new Audio("./sound/bug_pull.mp3");
+const winSound = new Audio("./sound/game_win.mp3");
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -40,6 +46,7 @@ function startGame() {
 	showStopButton();
 	showTimerAndScore();
 	startGameTimer();
+	playSound(bgSound);
 }
 
 function stopGame() {
@@ -47,11 +54,19 @@ function stopGame() {
 	stopGameTimer();
 	hideGameButton();
 	showPopUpWithText("REPLAY❓");
+	playSound(alertSound);
+	stopSound(bgSound);
 }
 
 function finishGame(win) {
 	started = false;
 	hideGameButton();
+	if (win) {
+		playSound(winSound);
+	} else {
+		playSound(bugSound);
+	}
+	stopSound(bgSound);
 	stopGameTimer();
 	showPopUpWithText(win ? "YOU WON 🎉" : "YOU LOST 💩");
 }
@@ -142,6 +157,7 @@ function onFieldClick(event) {
 	if (target.matches(".carrot")) {
 		target.remove();
 		score++;
+		playSound(carrotSound);
 		updateScoreBoard();
 		if (score === CARROT_COUNT) {
 			finishGame(true);
@@ -150,6 +166,14 @@ function onFieldClick(event) {
 		stopGameTimer();
 		finishGame(false);
 	}
+}
+
+function playSound(sound) {
+	sound.currentTime = 0;
+	sound.play();
+}
+function stopSound(sound) {
+	sound.pause();
 }
 
 function updateScoreBoard() {
