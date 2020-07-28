f666f262caebebf2ec9d3713d0172335889c594b
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-28 10:51:24 +0200

    LOGIC_REVIEW add functions to stop the game

--- carrot-game/main.js
+++ carrot-game/main.js
@@ -11,6 +11,10 @@ const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timer");
 const gameScore = document.querySelector(".game__score");
 
+const popUp = document.querySelector(".pop-up");
+const popUpText = document.querySelector(".pop-up__message");
+const popUpRefresh = document.querySelector(".pop-up__refresh");
+
 let started = false;
 let score = 0;
 let timer = undefined;
@@ -31,7 +35,11 @@ function startGame() {
 	startGameTimer();
 }
 
-function stopGame() {}
+function stopGame() {
+    stopGameTimer();
+    hideGameButton();
+	showPopUpWithText("REPLAY❓");
+}
 
 function showStopButton() {
 	const icon = gameBtn.querySelector(".fas");
@@ -39,6 +47,11 @@ function showStopButton() {
 	icon.classList.remove("fa-play");
 	gameBtn.style.visibility = "visible";
 }
+
+function hideGameButton(){
+    gameBtn.style.visibility = "hidden";
+}
+
 function showTimerAndScore() {
 	gameTimer.style.visibility = "visible";
 	gameScore.style.visibility = "visible";
@@ -56,10 +69,19 @@ function startGameTimer() {
 	}, 1000);
 }
 
+function stopGameTimer() {
+	clearInterval(timer);
+}
+
 function updateTimerText(time) {
 	const minutes = Math.floor(time / 60);
 	const seconds = time % 60;
 	gameTimer.innerText = `${minutes}:${seconds}`;
+}
+
+function showPopUpWithText (text){
+    popUpText.innerText = text;
+	popUp.classList.remove("pop-up--hide");
 }
 
 function initGame() {
