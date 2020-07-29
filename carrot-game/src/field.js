b77d5f599c404866afd78408a5b7be1374c774da
원다솜 <dasom@wondasom-ui-Air.fritz.box>
2020-07-29 19:00:33 +0200

    create freeze object to ensure type or case of item on field

--- carrot-game/src/field.js
+++ carrot-game/src/field.js
@@ -1,19 +1,24 @@
-"use strict";
-import * as sound from "./sound.js";
+'use strict';
+import * as sound from './sound.js';
 const CARROT_SIZE = 80;
 
-export default class Field {
+export const ItemType = Object.freeze({
+	carrot: 'carrot',
+	bug: 'bug'
+});
+
+export class Field {
 	constructor(carrotCount, bugCount) {
 		this.carrotCount = carrotCount;
 		this.bugCount = bugCount;
-		this.field = document.querySelector(".game__field");
+		this.field = document.querySelector('.game__field');
 		this.fieldRect = this.field.getBoundingClientRect();
-		this.field.addEventListener("click", this.onClick);
+		this.field.addEventListener('click', this.onClick);
 	}
 	init() {
-		this.field.innerHTML = "";
-		this._addItem("carrot", this.carrotCount, "img/carrot.png");
-		this._addItem("bug", this.bugCount, "img/bug.png");
+		this.field.innerHTML = '';
+		this._addItem('carrot', this.carrotCount, 'img/carrot.png');
+		this._addItem('bug', this.bugCount, 'img/bug.png');
 	}
 
 	setClickListener(onItemClick) {
@@ -26,10 +31,10 @@ export default class Field {
 		const x2 = this.fieldRect.width - CARROT_SIZE;
 		const y2 = this.fieldRect.height - CARROT_SIZE;
 		for (let i = 0; i < count; i++) {
-			const item = document.createElement("img");
-			item.setAttribute("class", className);
-			item.setAttribute("src", imgPath);
-			item.style.position = "absolute";
+			const item = document.createElement('img');
+			item.setAttribute('class', className);
+			item.setAttribute('src', imgPath);
+			item.style.position = 'absolute';
 			const x = randomNumber(x1, x2);
 			const y = randomNumber(y1, y2);
 			item.style.left = `${x}px`;
@@ -39,12 +44,12 @@ export default class Field {
 	}
 	onClick = (event) => {
 		const target = event.target;
-		if (target.matches(".carrot")) {
+		if (target.matches('.carrot')) {
 			target.remove();
 			sound.playCarrot();
-			this.onItemClick && this.onItemClick("carrot");
-		} else if (target.matches(".bug")) {
-			this.onItemClick && this.onItemClick("bug");
+			this.onItemClick && this.onItemClick(ItemType.carrot);
+		} else if (target.matches('.bug')) {
+			this.onItemClick && this.onItemClick(ItemType.bug);
 		}
 	};
 }
